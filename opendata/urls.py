4b52ff1c2f911742510ca809db72c390eb91d5c8
Ted Han <ted@knowtheory.net>
2015-02-18 11:44:52 -0600

    fixing domain regexp

--- opendata/urls.py
+++ opendata/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'lmgtfy.views.main_view', name='home'),
-    url(r'^(?P<domain>[.a-z]+)/$', 'lmgtfy.views.search_result_view', name='domain_result'),
-    url(r'^(?P<domain>[.a-z]+):(?P<fmt>\w+)/$', 'lmgtfy.views.search_result_view', name='domain_result_fmt'),
-    url(r'^(?P<domain>[.a-z]+)/csv$', 'lmgtfy.views.get_csv', name='domain_result_download'),
+    url(r'^(?P<domain>[.a-zA-Z0-9-]+)/$', 'lmgtfy.views.search_result_view', name='domain_result'),
+    url(r'^(?P<domain>[.a-zA-Z0-9-]+):(?P<fmt>\w+)/$', 'lmgtfy.views.search_result_view', name='domain_result_fmt'),
+    url(r'^(?P<domain>[.a-zA-Z0-9-]+)/csv$', 'lmgtfy.views.get_csv', name='domain_result_download'),
 )
