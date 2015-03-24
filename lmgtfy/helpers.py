4b3ef2ffd1d3759f6109a8cec022326735d5a48a
Waldo Jaquith <waldo@jaquith.org>
2015-03-23 20:52:32 -0400

    Remove unused Yahoo code, correct comment text

--- lmgtfy/helpers.py
+++ lmgtfy/helpers.py
@@ -8,24 +8,9 @@ from lmgtfy.tasks import search_bing_task
 class CleanSubmitButton(Submit):
     field_classes = 'btn btn-default'
 
-
-# def search_yahoo(domain):
-#     domain_db_record, _created = Domain.objects.get_or_create(name=domain)
-#     # currently we are do not allow to search the same domain more than once per day
-#     recently_searched = DomainSearch.objects.filter(
-#         created_at__gte=datetime.now()-timedelta(days=1),
-#         domain=domain_db_record
-#     ).count()
-#     if recently_searched:
-#         return False
-#     else:
-#         search_yahoo_task.apply_async(kwargs={'domain': domain})
-#         return True
-
-
 def search_bing(domain):
     domain_db_record, _created = Domain.objects.get_or_create(name=domain)
-    # currently we are do not allow to search the same domain more than once per day
+    # Bing does not allow us to search the same domain more than once per day.
     recently_searched = DomainSearch.objects.filter(
         created_at__gte=datetime.now()-timedelta(days=1),
         domain=domain_db_record
